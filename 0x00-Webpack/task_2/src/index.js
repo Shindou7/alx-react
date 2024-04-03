70dd0d984dc67d7357b916af43ee17756cdbb304
Shindou7 <sh.shindou@gmail.com>
2024-04-02 18:12:54 -0700

    g

---
+++ 0x00-Webpack/task_2/src/index.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+import './css/main.css';
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
